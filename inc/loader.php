03a9091b673d035e73df3247f94bf6d15b45f770
RahulTiwari2007 <rahul.tiwari@himanshusofttech.com>
2019-07-05 11:21:07 +0530

    Basic plugin setup

--- inc/loader.php
+++ inc/loader.php
@@ -5,4 +5,4 @@ require_once( FRESH_CONNECT_DIR_PATH . 'inc/WordPress/SessionStore.php' );
 require_once( FRESH_CONNECT_DIR_PATH . 'inc/AutoLogin.php' );
 require_once( FRESH_CONNECT_DIR_PATH . 'inc/WordPress/GetState.php' );
 require_once( FRESH_CONNECT_DIR_PATH . 'inc/Api.php' );
-require_once( FRESH_CONNECT_DIR_PATH . 'inc/Updater.php' );
+require_once( FRESH_CONNECT_DIR_PATH . 'inc/FC_Updater.php' );
