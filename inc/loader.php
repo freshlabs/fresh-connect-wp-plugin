083d06aa8fd8c08aa9c4041c19a64508b7e80939
RahulTiwari2007 <rahul.tiwari@himanshusofttech.com>
2019-07-05 11:12:57 +0530

    Basic plugin setup

--- inc/loader.php
+++ inc/loader.php
@@ -1,5 +1,5 @@
 <?php
-require_once( FRESH_CONNECT_DIR_PATH . 'inc/Class-fresh-connect.php' );
+require_once( FRESH_CONNECT_DIR_PATH . 'inc/ClassFreshConnect.php' );
 require_once( FRESH_CONNECT_DIR_PATH . 'inc/WordPress/Context.php' );
 require_once( FRESH_CONNECT_DIR_PATH . 'inc/WordPress/SessionStore.php' );	
 require_once( FRESH_CONNECT_DIR_PATH . 'inc/AutoLogin.php' );
