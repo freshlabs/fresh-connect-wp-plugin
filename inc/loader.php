a887903e5e5e28e2e1b28a20c8c61fe9ccca9b90
RahulTiwari2007 <rahul.tiwari@himanshusofttech.com>
2019-07-10 11:23:34 +0530

    Basic plugin setup

--- inc/loader.php
+++ inc/loader.php
@@ -6,4 +6,3 @@ require_once( FRESH_CONNECT_DIR_PATH . 'inc/AutoLogin.php' );
 require_once( FRESH_CONNECT_DIR_PATH . 'inc/tgm-plugins.php' );
 require_once( FRESH_CONNECT_DIR_PATH . 'inc/WordPress/GetState.php' );
 require_once( FRESH_CONNECT_DIR_PATH . 'inc/Api.php' );
-require_once( FRESH_CONNECT_DIR_PATH . 'inc/FC_Updater.php' );
